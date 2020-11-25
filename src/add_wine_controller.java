6d8014e2e285337c4833425833f43be22b1284db
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-25 19:31:52 +0100

    add wine controller stuff

--- src/add_wine_controller.java
+++ src/add_wine_controller.java
@@ -1,3 +1,64 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+
 public class add_wine_controller {
-    
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField year;
+
+    @FXML
+    private TextField producer;
+
+    @FXML
+    private TextField grapes;
+
+    @FXML
+    private TextArea notes;
+
+    @FXML
+    void addWine(ActionEvent event) {
+        String nam = name.getText();
+        String yea_tmp = year.getText();
+        String pro = producer.getText();
+        String gra = grapes.getText();
+        String not = notes.getText();
+
+        if(nam.length() == 0 || yea_tmp.length() == 0 || pro.length() == 0 ||
+         gra.length() == 0 || not.length() == 0){
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("All fields must be filled");
+            alert.setHeaderText("Please fill all the fields");
+            alert.showAndWait();
+        }
+
+        try {
+            int yea = Integer.parseInt(yea_tmp);
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid Year");
+            alert.setHeaderText("Please insert a valid year.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    void back(ActionEvent event) throws IOException{
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_employee.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
 }
