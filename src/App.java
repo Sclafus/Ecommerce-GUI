927787e698d63b7b37a96adefebcc81d678d634f
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-11-25 13:54:08 +0100

    added email regex

--- src/App.java
+++ src/App.java
@@ -10,7 +10,7 @@ public class App extends Application{
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("./login.fxml"));
         
         Scene scene = new Scene(root);
 
--- src/registerController.java
+++ src/registerController.java
@@ -2,18 +2,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+
 
 public class registerController implements Initializable{
     
@@ -40,6 +41,25 @@ public class registerController implements Initializable{
 
     }
 
+    /**
+     * Checks if the provided String is an email or not. This method
+     * uses RegEx. 
+     * 
+     * @param mail the mail when need to check. [String]
+     * @return true if the string is an email, else false. [Boolean]
+     * 
+     * @see java.util.regex.Pattern
+     * @see java.util.regex.Matcher
+     */
+    public Boolean isMail(String mail){
+        
+        String mail_regex = "\\w+@\\w+\\.\\w+";
+        Pattern mail_validator = Pattern.compile(mail_regex);
+        Matcher mail_matcher = mail_validator.matcher(mail);
+        return mail_matcher.matches();
+        
+    }
+
     @FXML
     void newUser(ActionEvent event) {
 
@@ -47,21 +67,23 @@ public class registerController implements Initializable{
         String sur = surname.getText();
         String mail = email.getText();
         String pass = password.getText();
-        String mail_regex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$";
-        Pattern pattern = Pattern.compile(mail_regex);
+
+        //The flag value is used to not prompt 2 alerts at the same time.
+        Boolean flag = true;
 
         if( nam.length() == 0|| sur.length() == 0 || mail.length() == 0 || pass.length() == 0){
+            flag = !flag;
             Alert alert = new Alert(AlertType.WARNING);
             alert.setTitle("All fields must be filled");
             alert.setHeaderText("Please fill all the fields");
             alert.showAndWait();
         }
 
-        //TODO: FIX REGEX FOR MAIL
-        if(mail_regex.matches(mail)){
-            System.out.println("good job");
-        } else {
-            System.out.println("lol");
+        if(!isMail(mail) && flag){
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Email not valid");
+            alert.setHeaderText("The provided email is not valid, please retry.");
+            alert.showAndWait();
         }
 
         //TODO: add new user to DB
