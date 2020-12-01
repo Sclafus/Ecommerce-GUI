6ff5e06fb7ce6a5c95d50e37a7ecbc0de7c7c66b
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-01 14:58:40 +0100

    login and register methods

--- src/login_controller.java
+++ src/login_controller.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
@@ -37,51 +41,58 @@ public class login_controller {
 		//gets the informations
 		String mail = email.getText();
 		String pass = password.getText();
-		Boolean sent_alert = false;
 
 		//socket stuff
 		try{
+
 			//client -> server
 			Socket socket = new Socket("localhost", 4316);
-			PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-			printWriter.println(mail);
+			if(mail.length() > 0 && pass.length() > 0){
+				OutputStream outputStream = socket.getOutputStream();
+				ObjectOutputStream out = new ObjectOutputStream(outputStream);
+				String[] to_be_sent = {"login", mail, pass};
+				out.writeObject(to_be_sent);
 
-			//server -> client
-			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String response = bufferReader.readLine();
-			System.out.println(response);
-			socket.close();
+				//server -> client
+				InputStream inputStream = socket.getInputStream();
+				ObjectInputStream in = new ObjectInputStream(inputStream);
+				int permission = (int) in.readObject();
+
+				switch (permission) {
+					case 1:
+						AnchorPane pane1 = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
+						rootPane.getChildren().setAll(pane1);
+						break;
+					case 2:
+						AnchorPane pane2 = FXMLLoader.load(getClass().getResource("./homepage_employee.fxml"));
+						rootPane.getChildren().setAll(pane2);
+						break;
+					case 3:
+						AnchorPane pane3 = FXMLLoader.load(getClass().getResource("./homepage_admin.fxml"));
+						rootPane.getChildren().setAll(pane3);
+						break;
+					default:
+						AnchorPane pane_default = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
+						rootPane.getChildren().setAll(pane_default);
+						break;
+				}
+
+				socket.close();
+			} else {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("All fields must be filled");
+				alert.setHeaderText("Please fill all the fields");
+				alert.showAndWait();
+			}
+
+
 		} catch(ConnectException e){
-			sent_alert = true;
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Cannot connect to server");
 			alert.setHeaderText("Server is unreachable. Try again later.");
 			alert.showAndWait();
-		}
-
-		
-		if((mail.length() == 0 || pass.length() == 0) && !sent_alert){
-			Alert alert = new Alert(AlertType.WARNING);
-			alert.setTitle("All fields must be filled");
-			alert.setHeaderText("Please fill all the fields");
-			alert.showAndWait();
-		}
-
-		//tmp stuff
-		Boolean isAdmin = true;
-		Boolean isEmployee = false;
-		//~tmp stuff
-		
-		
-		if (isAdmin){
-			AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_admin.fxml"));
-			rootPane.getChildren().setAll(pane);
-		} else if(isEmployee){
-			AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_employee.fxml"));
-			rootPane.getChildren().setAll(pane);
-		} else {
-			AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
-			rootPane.getChildren().setAll(pane);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 
 	}
--- src/register_controller.java
+++ src/register_controller.java
@@ -1,6 +1,11 @@
 
 // import java.io.IOException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
@@ -17,15 +22,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
+public class register_controller implements Initializable {
 
-public class register_controller implements Initializable{
-    
     @FXML
     private AnchorPane rootPane;
 
     @FXML
     private TextField name;
-    
+
     @FXML
     private TextField surname;
 
@@ -39,13 +43,12 @@ public class register_controller implements Initializable{
     private Text message;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
 
     }
 
     /**
-     * Checks if the provided String is an email or not. This method
-     * uses RegEx. 
+     * Checks if the provided String is an email or not. This method uses RegEx.
      * 
      * @param mail the mail when need to check. [String]
      * @return true if the string is an email, else false. [Boolean]
@@ -53,13 +56,13 @@ public class register_controller implements Initializable{
      * @see java.util.regex.Pattern
      * @see java.util.regex.Matcher
      */
-    public Boolean isMail(String mail){
-        
+    public Boolean isMail(String mail) {
+
         String mail_regex = "\\w+@\\w+\\.\\w+";
         Pattern mail_validator = Pattern.compile(mail_regex);
         Matcher mail_matcher = mail_validator.matcher(mail);
         return mail_matcher.matches();
-        
+
     }
 
     @FXML
@@ -70,25 +73,36 @@ public class register_controller implements Initializable{
         String mail = email.getText();
         String pass = password.getText();
 
-        //The flag value is used to not prompt 2 alerts at the same time.
-        Boolean flag = true;
-
-        if(nam.length() == 0 || sur.length() == 0 || mail.length() == 0 || pass.length() == 0){
-            flag = false;
+        if (nam.length() == 0 || sur.length() == 0 || mail.length() == 0 || pass.length() == 0) {
             Alert alert = new Alert(AlertType.WARNING);
             alert.setTitle("All fields must be filled");
             alert.setHeaderText("Please fill all the fields");
             alert.showAndWait();
-        }
-
-        if(!isMail(mail) && flag){
+        } else if (!isMail(mail)) {
             Alert alert = new Alert(AlertType.WARNING);
             alert.setTitle("Email not valid");
             alert.setHeaderText("The provided email is not valid, please retry.");
             alert.showAndWait();
-        }
+        } else {
+            Socket socket = new Socket("localhost", 4316);
 
-        //TODO: add new user to DB
+            OutputStream outputStream = socket.getOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(outputStream);
+            String[] to_be_sent = { "register", nam, sur, mail, pass };
+            out.writeObject(to_be_sent);
+
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream in = new ObjectInputStream(inputStream);
+
+            
+            try {
+                int permission = (int) in.readObject();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            
+            socket.close();
+        }
 
         //TODO: login
         Boolean login_flag=true;
