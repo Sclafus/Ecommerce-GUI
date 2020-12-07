4f57ee8eefdd58d197624696ce99ca5d9f446fd0
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-07 15:59:58 +0100

    admin can now see employees in a treeview [epic] Co-authored-by: marticaffa

--- src/ControllerHomepageAdmin.java
+++ src/ControllerHomepageAdmin.java
@@ -5,19 +5,22 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class ControllerHomepageAdmin implements Controller {
 
 	private User current_user;
-	
+
 	@FXML
 	private AnchorPane rootPane;
 
@@ -26,15 +29,15 @@ public class ControllerHomepageAdmin implements Controller {
 
 	@FXML
 	private Text name;
-	
-	public void initData(User user){
+
+	public void initData(User user) {
 		this.current_user = user;
-		this.name.setText(this.current_user.getName()); 
+		this.name.setText(this.current_user.getName());
 	}
-	
+
 	/**
-	 * loads add_employee.fxml
-	 * Adds a new employee to the database:
+	 * loads add_employee.fxml Adds a new employee to the database:
+	 * 
 	 * @param event button event on the GUI.
 	 * @throws IOException if the client can't open the file.
 	 */
@@ -46,23 +49,42 @@ public class ControllerHomepageAdmin implements Controller {
 
 	/**
 	 * It displays all the employees.
+	 * 
 	 * @param event button event on the GUI.
 	 * @throws IOException if the client can't open a connection with the server.
 	 * @see User
 	 */
-	// TODO use output
+	// TODO Fix javadoc
 	@FXML
 	void displayEmployees(ActionEvent event) throws IOException {
-			Socket socket = new Socket("localhost", 4316);
+		Socket socket = new Socket("localhost", 4316);
 
-			OutputStream outputStream = socket.getOutputStream();
-			ObjectOutputStream out = new ObjectOutputStream(outputStream);
-			String[] to_be_sent = {"get_employees"};
-			out.writeObject(to_be_sent);
+		OutputStream outputStream = socket.getOutputStream();
+		ObjectOutputStream out = new ObjectOutputStream(outputStream);
+		String[] to_be_sent = {"get_employees"};
+		out.writeObject(to_be_sent);
 
-			InputStream inputStream = socket.getInputStream();
-			ObjectInputStream in = new ObjectInputStream(inputStream);
-			
+		InputStream inputStream = socket.getInputStream();
+		ObjectInputStream in = new ObjectInputStream(inputStream);
+
+		try {
+			ArrayList<User> employees = (ArrayList<User>) in.readObject();
+			TreeItem rootItem = new TreeItem("Employees");
+
+			for (User employee : employees){
+				TreeItem<String> rootEmployee = new TreeItem<String> (employee.getEmail());
+				TreeItem<String> name = new TreeItem<String> ("Name: " + employee.getName());
+				TreeItem<String> surname = new TreeItem<String> ("Surname: " + employee.getSurname());
+				rootEmployee.getChildren().addAll(name,surname);
+				rootItem.getChildren().add(rootEmployee);
+			}
+			rootItem.setExpanded(true);
+			treeView.setRoot(rootItem);
+
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 			socket.close();
 		}
 	
