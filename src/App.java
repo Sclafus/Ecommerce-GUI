94d72e4a9a31734340ab928c16bd3ed424f1db50
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-08 12:51:20 +0100

    javadoc

--- src/App.java
+++ src/App.java
@@ -1,22 +1,38 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class App extends Application{
-	
 
+/**
+ * App class, used to start the GUI.
+ */
+public class App extends Application {
+
+	/**
+	 * Starts the GUI.
+	 * 
+	 * @param stage the GUI stage. [Stage]
+	 * @throws IOException if the file cannot be read.
+	 */
 	@Override
-	public void start(Stage stage) throws Exception{
+	public void start(Stage stage) throws IOException {
 
 		Parent root = FXMLLoader.load(getClass().getResource("./login.fxml"));
-		
+
 		Scene scene = new Scene(root);
 		stage.setTitle("3rd Assignment");
 		stage.setScene(scene);
 		stage.show();
 	}
-	public static void main(String[] args){
+
+	/**
+	 * Main method.
+	 * @param args argvs, actually not required for our use.
+	 */
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
