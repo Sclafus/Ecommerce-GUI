a98522919d8c694a0f1e164ec5d224fbdbee0917
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-08 16:23:07 +0100

    id has to be entered manually, added method to restock wines

--- src/ControllerRestock.java
+++ src/ControllerRestock.java
@@ -1,8 +1,16 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class ControllerRestock implements Controller {
@@ -13,28 +21,79 @@ public class ControllerRestock implements Controller {
 	private AnchorPane rootPane;
 
 	@FXML
-	private MenuButton wine;
+	private TextField id;
 
-	
+	@FXML
+	private TextField quantity;
+
 	public void initData(User user) {
 		this.current_user = user;
 	}
 
-	@FXML
-	void restockWine(ActionEvent event) {
-		//TODO
+	@FXML @SuppressWarnings("unused")
+	void restockWine(ActionEvent event) throws UnknownHostException, IOException {
+		if (this.current_user.getPermission() > 1) {
+			// user is authorized to perform the action
+
+			try {
+				int id_int = Integer.parseInt(id.getText());
+				int quantity_int = Integer.parseInt(quantity.getText());
+
+				// client -> server
+				Socket socket = new Socket("localhost", 4316);
+				OutputStream outputStream = socket.getOutputStream();
+				ObjectOutputStream out = new ObjectOutputStream(outputStream);
+				String[] to_be_sent = { "restock_wine", id.getText(), quantity.getText() };
+				out.writeObject(to_be_sent);
+
+				// server -> client
+				InputStream inputStream = socket.getInputStream();
+				ObjectInputStream in = new ObjectInputStream(inputStream);
+				Boolean restocked = (Boolean) in.readObject();
+
+				if (restocked) {
+					// success
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Success");
+					alert.setHeaderText("Wine has been restocked successfully.");
+					alert.showAndWait();
+				} else {
+					// failure
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Failed");
+					alert.setHeaderText("Something went wrong. Are you sure the ID is correct?");
+					alert.showAndWait();
+				}
+				socket.close();
+
+			} catch (NumberFormatException e) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Not authorized");
+				alert.setHeaderText("You are not allowed to perform this action.");
+				alert.showAndWait();
+
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			// user is not authorized to perform the action
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Not authorized");
+			alert.setHeaderText("You are not allowed to perform this action.");
+			alert.showAndWait();
+		}
 	}
 
 	@FXML
 	void back(ActionEvent event) throws IOException {
 		Loader loader = new Loader(this.current_user, this.rootPane);
 		loader.load("homepage_employee");
-    }
+	}
 
-    @FXML
-    void setText(ActionEvent event) {
-		//TODO FIGURE OUT WHAT THIS IS
-    }
+	@FXML
+	void setText(ActionEvent event) {
+		// TODO FIGURE OUT WHAT THIS IS
+	}
 
 }
-
