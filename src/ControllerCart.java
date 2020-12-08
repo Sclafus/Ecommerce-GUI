8760cef7a75f2a5c901208d13a69b8ca29b96b17
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-08 14:25:15 +0100

    javadoc and formatting

--- src/ControllerCart.java
+++ src/ControllerCart.java
@@ -2,39 +2,47 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
+//TODO javadoc
 public class ControllerCart implements Controller {
 
 	private User current_user;
-	
+
 	@FXML
 	private AnchorPane rootPane;
-	
+
 	@FXML
 	private TreeView<String> treeview;
 
+	/**
+	 * Initialize {@code this.current_user} with the passed value. This method is
+	 * made to be called from another controller, using the {@code load} method in
+	 * {@code Loader} class.
+	 * 
+	 * @param user the {@code User} we want to pass. [User]
+	 * @see Loader
+	 */
+	public void initData(User user) {
+		this.current_user = user;
+	}
+
 	@FXML
 	void back(ActionEvent event) throws IOException {
-		AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
-		rootPane.getChildren().setAll(pane);
+		Loader loader = new Loader(this.current_user, this.rootPane);
+		loader.load("homepage_user");
 	}
 
 	@FXML
 	void buy(ActionEvent event) {
+		// TODO
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Order submitted!");
 		alert.setHeaderText("Your order has been placed!");
 		alert.showAndWait();
-	}
-
-	public void initData(User user){
-		current_user = user;
-		System.out.println(current_user.getEmail() + current_user.getPermission());
 	}
 
 }
