e13d782942be8b619f71db657dfbeee7f044f4b3
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-08 14:26:20 +0100

    javadoc, formatting, using Loader class, changed methods names

--- src/ControllerHomepageEmployee.java
+++ src/ControllerHomepageEmployee.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
+//TODO javadoc
 public class ControllerHomepageEmployee implements Controller {
 
 	private User current_user;
@@ -20,45 +21,65 @@ public class ControllerHomepageEmployee implements Controller {
 
 	@FXML
 	private TextField searchID;
-	
+
 	@FXML
 	private Text name;
 
-	public void initData(User user){
+	/**
+	 * Initialize {@code this.current_user} with the passed value. This method is
+	 * made to be called from another controller, using the {@code load} method in
+	 * {@code Loader} class.
+	 * 
+	 * @param user the {@code User} we want to pass. [User]
+	 * @see Loader
+	 */
+	public void initData(User user) {
 		this.current_user = user;
 		name.setText(this.current_user.getName());
 	}
 
+	/**
+	 * Goes to page for adding new {@code Wine}s.
+	 * 
+	 * @param event GUI event. [ActionEvent]
+	 * @throws IOException if the file can't be accessed.
+	 */
 	@FXML
-	void addWine(ActionEvent event) throws IOException {
+	void loadAddWine(ActionEvent event) throws IOException {
 		Loader loader = new Loader(this.current_user, this.rootPane);
 		loader.load("add_wine");
 	}
 
 	@FXML
 	void display(ActionEvent event) {
-		
+		// TODO
 	}
 
 	@FXML
-	void loadShop(ActionEvent event) throws IOException{
-		AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_user.fxml"));
-		this.rootPane.getChildren().setAll(pane);
+	void loadShop(ActionEvent event) throws IOException {
+		Loader loader = new Loader(this.current_user, this.rootPane);
+		loader.load("homepage_user");
 	}
 
 	@FXML
-	void restockWine(ActionEvent event) throws IOException {
-		AnchorPane pane = FXMLLoader.load(getClass().getResource("./restock.fxml"));
-		this.rootPane.getChildren().setAll(pane);
+	void loadRestockWine(ActionEvent event) throws IOException {
+		Loader loader = new Loader(this.current_user, this.rootPane);
+		loader.load("restock");
 	}
 
 	@FXML
 	void shipOrder(ActionEvent event) {
 
 	}
-	
+
+	/**
+	 * Goes back to the login page.
+	 * 
+	 * @param event GUI event. [ActionEvent]
+	 * @throws IOException if the filename cannot be read.
+	 */
 	@FXML
-	void logout(ActionEvent event) throws IOException{
+	void logout(ActionEvent event) throws IOException {
 		AnchorPane pane = FXMLLoader.load(getClass().getResource("./login.fxml"));
 		this.rootPane.getChildren().setAll(pane);
 	}
