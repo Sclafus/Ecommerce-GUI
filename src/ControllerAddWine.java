216c9c55d5e6baf72ce07065d007ef976c6d149d
Alessandro Sclafani <alessandro.sclafani00@gmail.com>
2020-12-08 12:32:33 +0100

    javadoc

--- src/ControllerAddWine.java
+++ src/ControllerAddWine.java
@@ -8,7 +8,6 @@ import java.net.UnknownHostException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class ControllerAddWine implements Controller {
+
 	private User current_user;
 
 	@FXML
@@ -36,10 +36,24 @@ public class ControllerAddWine implements Controller {
 	@FXML
 	private TextArea notes;
 
-	public void initData(User user){
+	/**
+	 * Initialize {@code this.current_user} with the passed value.
+	 * This method is made to be called from another controller,
+	 * using the {@code load} method in {@code Loader} class.
+	 * @param user we want to pass. [User]
+	 * @see Loader
+	 */
+	public void initData(User user) {
 		this.current_user = user;
 	}
 
+	/**
+	 * 
+	 * @param event GUI event. [ActionEvent]
+	 * @throws UnknownHostException if the IP address of the host could not be
+	 *                              determined.
+	 * @throws IOException          if an I/O error occurs when creating the socket.
+	 */
 	@FXML
 	public void addWine(ActionEvent event) throws UnknownHostException, IOException {
 		int yea = 0;
@@ -49,48 +63,57 @@ public class ControllerAddWine implements Controller {
 		String gra = grapes.getText();
 		String not = notes.getText();
 
-		if(nam.length() == 0 || yea_tmp.length() == 0 || pro.length() == 0 ||
-		 gra.length() == 0 || not.length() == 0){
+		if (nam.length() == 0 || yea_tmp.length() == 0 || pro.length() == 0 || gra.length() == 0 || not.length() == 0) {
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("All fields must be filled");
 			alert.setHeaderText("Please fill all the fields");
 			alert.showAndWait();
 		} else {
-			try{
+			try {
 				yea = Integer.parseInt(yea_tmp);
-			} catch (NumberFormatException e){
+			} catch (NumberFormatException e) {
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setTitle("Invalid Year");
 				alert.setHeaderText("Please insert a valid year.");
 				alert.showAndWait();
-			} finally{
+			} finally {
 				Socket socket = new Socket("localhost", 4316);
 
 				OutputStream outputStream = socket.getOutputStream();
 				ObjectOutputStream out = new ObjectOutputStream(outputStream);
-				String[] to_be_sent  = {"add_wine", nam, yea_tmp, pro, gra, not};
+				String[] to_be_sent = { "add_wine", nam, yea_tmp, pro, gra, not };
 				out.writeObject(to_be_sent);
-	
+
 				InputStream inputStream = socket.getInputStream();
 				ObjectInputStream in = new ObjectInputStream(inputStream);
-	
-				
+
 				try {
-					int permission = (int) in.readObject();
+					Wine new_wine = (Wine) in.readObject();
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Wine added!");
+					alert.setHeaderText(String.format("Wine %s %d by %s has been added", new_wine.getName(),
+							new_wine.getYear(), new_wine.getProducer()));
+					alert.showAndWait();
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 				}
-				
+
 				socket.close();
 			}
-			
+
 		}
 	}
 
+	/**
+	 * Goes back to the employee homepage.
+	 * 
+	 * @param event GUI event. [ActionEvent]
+	 * @throws IOException if the filename cannot be read.
+	 */
 	@FXML
-	public void back(ActionEvent event) throws IOException{
-		AnchorPane pane = FXMLLoader.load(getClass().getResource("./homepage_employee.fxml"));
-		rootPane.getChildren().setAll(pane);
+	public void back(ActionEvent event) throws IOException {
+		Loader loader = new Loader(this.current_user, this.rootPane);
+		loader.load("homepage_employee");
 	}
 
 }
