26779026b568a33d8cdee8679bc2c373739a9fa9
Martina Caffagnini <martina.caffagnini@gmail.com>
2020-12-09 09:59:08 +0100

    boh

--- src/ControllerOrders.java
+++ src/ControllerOrders.java
@@ -1,9 +1,18 @@
 import java.io.IOException;
+import java.net.UnknownHostException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 
 /**
  * Controller for Orders, page accessible by {@code User} 
@@ -41,6 +50,47 @@ public class ControllerOrders implements Controller {
 	 */
 	public void initData(User user){
 		this.current_user = user;
-		//TODO fill the treeview
+		fillTreeView();
+	}
+
+	/**
+	 * Displays all the employees in the TreeView.
+	 * 
+	 * @param event GUI event. [ActionEvent]
+	 * @throws UnknownHostException if the IP address of the host could not be
+	 *                              determined.
+	 * @throws IOException          if an I/O error occurs when creating the socket.
+	 * @see User
+	 */
+	public void fillTreeView(ActionEvent event) throws IOException, UnknownHostException{
+		
+		if (this.current_user.getPermission() > 0) {
+			// user is authorized to perform the action
+			Socket socket = new Socket("localhost", 4316);
+
+			// client -> server
+			OutputStream outputStream = socket.getOutputStream();
+			ObjectOutputStream out = new ObjectOutputStream(outputStream);
+			String[] to_be_sent = { "get_orders_user", this.current_user.getEmail() };
+			out.writeObject(to_be_sent);
+
+			// server -> client
+			InputStream inputStream = socket.getInputStream();
+			ObjectInputStream in = new ObjectInputStream(inputStream);
+
+			try{
+
+			}catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			socket.close();
+		} else {
+
+			// user is not authorized to perform the action
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Not authorized");
+			alert.setHeaderText("You are not allowed to perform this action.");
+			alert.showAndWait();
+		}
 	}
 }
